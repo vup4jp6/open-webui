c70aab7cb037913659c8deea6394324c6df1fcb3
momo <vup4jp6@gmail.com>
2024-07-19 10:10:54 +0800

    modify make2real

--- backend/main.py
+++ backend/main.py
@@ -81,19 +81,19 @@ class SPAStaticFiles(StaticFiles):
                 raise ex
 
 
+#   ___                    __        __   _     _   _ ___ 
+#  / _ \ _ __   ___ _ __   \ \      / /__| |__ | | | |_ _|
+# | | | | '_ \ / _ \ '_ \   \ \ /\ / / _ \ '_ \| | | || | 
+# | |_| | |_) |  __/ | | |   \ V  V /  __/ |_) | |_| || | 
+#  \___/| .__/ \___|_| |_|    \_/\_/ \___|_.__/ \___/|___|
+#       |_|   
+
 print(
     rf"""
-  ___                    __        __   _     _   _ ___ 
- / _ \ _ __   ___ _ __   \ \      / /__| |__ | | | |_ _|
-| | | | '_ \ / _ \ '_ \   \ \ /\ / / _ \ '_ \| | | || | 
-| |_| | |_) |  __/ | | |   \ V  V /  __/ |_) | |_| || | 
- \___/| .__/ \___|_| |_|    \_/\_/ \___|_.__/ \___/|___|
-      |_|                                               
-
       
 v{VERSION} - building the best open-source AI user interface.
 {f"Commit: {WEBUI_BUILD_HASH}" if WEBUI_BUILD_HASH != "dev-build" else ""}
-https://github.com/open-webui/open-webui
+https://make2real.ai/
 """
 )
 
